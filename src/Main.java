fb2ea4a9505d9592d19e68bae392dbc0501a1e5b
Kirillova Olesya <olesyak-85@mail.ru>
2021-08-09 23:52:04 +0300

    Add Report.md

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         int account = 2_000_000_000;
-        int remittance = 500_000;
+        int remittance = 500_000_000;
         int balance = account + remittance;
         System.out.println("balance");
 
