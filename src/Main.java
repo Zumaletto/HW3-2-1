d8d9f6038a4436da15fc474859553788b32d5c27
Kirillova Olesya <olesyak-85@mail.ru>
2021-08-09 22:41:33 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 2_000_000_000;
+        int remittance = 500_000;
+        int balance = account + remittance;
+        System.out.println("balance");
+
+    }
+}
